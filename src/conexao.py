569fb80c5cdfc64e66c58e65fd77759bc1615678
Kauezinxl <168539844+Kauezinxl@users.noreply.github.com>
2024-10-03 09:25:55 -0300

    conexao.py

--- src/conexao.py
+++ src/conexao.py
@@ -1,14 +1,35 @@
-import mysql
-import mysql.connector
+from conexao import *
 
-conn = mysql.connector.connect(
-    username = 'kaue',
-    host = 'localhost',
-    password = 'projeto123',
-    db = 'projeto_crud'
-)
+def enviar_dados(nome, email, senha):
+    criar_usuario(nome, email, senha)
 
-if conn.is_connected():
-    print('Banco de dados conectado com sucesso!')
-else:
-    print('Não conectado com o banco!')
+
+def criar_usuario(nome, email, senha):
+    if conn.is_connected():
+        print('Banco conectado com sucesso!')
+        
+
+        cursor = conn.cursor()
+
+        sql = "INSERT INTO usuario (nome, email, senha) values (%s, %s, %s)"       
+        values = (nome, email, senha)
+
+        cursor.execute(sql, values)
+        conn.commit()
+        conn.close()
+        cursor.close()
+    else:
+        print('Falha ao conectar com o servidor!')
+
+def lista_usuario():
+    if conn.is_connected():
+         print('Banco de dados conectado com sucesso!')
+        
+         cursor = conn.cursor()
+
+         cursor.execute('Select id, nome, email from usuario;')
+
+         usuarios = cursor.fetchall()
+         return usuarios
+    else:
+         print('Falha ao conectar com o banco de dados!')
