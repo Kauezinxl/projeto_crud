c6b0266bb0944c5ac6d646eeffc599a9e304dbe2
Kauezinxl <168539844+Kauezinxl@users.noreply.github.com>
2024-10-03 09:23:57 -0300

    app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 import services
 
 def main():
@@ -10,47 +11,76 @@ def main():
 
         nomeEntry.delete(0, END)
         emailEntry.delete(0, END)
-        senhaEntry.delete(0, END)
+        senhaEntry.delete(0, END) 
+
+    def listar_usuario():
+        usuarios = services.lista_usuario()
+
+        janela_listar = Toplevel(janela)
+        janela_listar.title("Lista de Usuários")
+        janela_listar.geometry("600x300")
+
+        #Criar uma Treeview(view, vizualização) da lista de usuários, show = "headings" para limpar o cabeçalho
+
+        tree = ttk.Treeview(janela_listar, columns=("ID", "Nome", "Email"), show = "headings")
+        tree.heading("ID", text = "ID")
+        tree.heading("Nome", text = "Nome")
+        tree.heading("Email", text = "Email")
+
+        #criar um botão de voltar que irá fechar a tela de lista de usuários
+
+        voltar = Button(janela_listar, text = "Voltar", command = janela_listar.destroy)
+        voltar.pack(fill = BOTH, expand = True, side = BOTTOM)
+
+        tree.pack(fill = BOTH, expand = True)
+
+        #Inserir os dados dos usuários na Treeview
+
+        for usuario in usuarios:
+            #END vai inserir o item no final da tabela
+            tree.insert("", END, values = usuario)
+
 
     janela = Tk()
-    janela.geometry('400x300')
-    janela.title('Sistema de Gerenciamento de Usuario')
+    janela.geometry("400x400")
+    janela.title("Sistema de Gerenciamento de Usuário")
 
-    titulo = Label(janela, text='CRUD', font=('Arial', 20))
-    titulo.pack(pady=30)
 
-    # Componentes de entrada
-    # Nome
-    nome = Label(janela, text='Nome:')
-    nome.place(x=50, y=100)
+    #Titulo
+    titulo = Label(janela, text = "Crud")
+    titulo.pack(pady = 60)
+
+    #Nome
+    nome = Label(janela, text = "Nome: ")
+    nome.place(x = 40, y = 100)
 
     global nomeEntry
-    nomeEntry = Entry(janela, width=30)
-    nomeEntry.place(x=100, y=100)
+    nomeEntry = Entry(janela, width = 30, text = "Nome: ")
+    nomeEntry.place(x = 100, y = 100)
 
-    # EmaiL
-    email = Label(janela, text='Email:')
-    email.place(x=50, y=130)
+    #Email
+    email = Label(janela, text = "Email: ")
+    email.place(x = 40, y = 130)
 
-    emailEntry = Entry(janela, width=30)
-    emailEntry.place(x=100, y=130)
+    global emailEntry
+    emailEntry = Entry(janela, width = 30, text = "Email: ")
+    emailEntry.place(x = 100, y = 130)
 
-    # Senha
-    senha = Label(janela, text='Senha:')
-    senha.place(x=50, y=160)
+    #Senha
+    senha = Label(janela, text = "Senha: ")
+    senha.place(x = 40, y = 160)
 
-    # Comando show para esconder a senha
     global senhaEntry
-    senhaEntry = Entry(janela, width=30, show='*')
-    senhaEntry.place(x=100, y=160)
+    senhaEntry = Entry(janela, text = "Senha: ", width = 30, show = "*")
+    senhaEntry.place(x = 100, y = 160)
 
-    cadastrar = Button(janela, text='Cadastrar', width=10, command=on_enviar)
-    cadastrar.place(x=100, y=200)
+    cadastrar = Button(janela, text = "Cadastrar", width = 15, command = on_enviar)
+    cadastrar.place(x = 100, y = 200)
 
-    lista = Button(janela, text='Listar Usuários', width=15)
-    lista.place(x=200, y=200)
+    listar = Button(janela, text = "Listar Usuários", width = 15, command = listar_usuario)
+    listar.place(x = 250, y = 200)
 
     janela.mainloop()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
